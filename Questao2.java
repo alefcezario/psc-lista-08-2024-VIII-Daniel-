b0de33c569ca07c2a38d8418998cc1fe269dbca9
Alef Cezario <161400415+alefcezario@users.noreply.github.com>
2024-04-20 17:51:50 -0300

    Create Questao2.java
    2. Faça um programa para imprimir:
    1
    1 2
    1 2 3
    .....
    1 2 3 ... n
    
    para um n informado pelo usuário.
    Use uma função que receba um valor n inteiro imprima até a n-ésima linha.

---
+++ Questao2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Questao2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n;
+		
+		System.out.println("Digite o valor de n: ");
+		n = scanner.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+		    
+		    for (int j = 1; j <= i; j++) {
+		    System.out.print(j + " "); 
+		     
+		 }
+		 System.out.println();
+		}
+	
+		scanner.close();
+	}
+}
